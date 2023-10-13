77a0042dc8e43caf4e1f0a9669f6adb01bedac77
Ateeth Kosuri <ateethkosuri@gmail.com>
2023-10-13 11:32:25 -0700

    chore: add react query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,22 +3,22 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { ClerkProvider } from '@clerk/nextjs';
 
+import ReactQueryProvider from './utils/ReactQueryProvider';
+
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
 	title: 'Next Full Stack',
-	description: 'Template for generating fullstack NextJS apps',
+	description: 'Template for generating fullstack NextJS apps'
 };
 
-export default function RootLayout({
-	children
-}: {
-	children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<ClerkProvider>
 			<html lang='en'>
-				<body className={inter.className}>{children}</body>
+				<body className={inter.className}>
+					<ReactQueryProvider>{children}</ReactQueryProvider>
+				</body>
 			</html>
 		</ClerkProvider>
 	);
